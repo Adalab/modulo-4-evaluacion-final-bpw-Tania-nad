18ad0d83dbfd1e2657e22653222bb7fd7756c48e
Tania <nadsanp@gmail.com>
2024-12-20 13:45:36 +0100

    tried .env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 //Crear el servidor
 const api = express();
@@ -8,6 +9,9 @@ const api = express();
 //Configurar el servidor
 api.use(cors());
 api.use(express.json());
+
+
+
 
 //Escuchar el puerto
 const port = 4001;
